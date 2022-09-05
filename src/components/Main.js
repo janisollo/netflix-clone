082d8665f879f5c84950d7cd466b07b354928a5b
janisollo <sollo.jani@gmail.com>
2022-09-05 08:48:27 +0700

    Axios get for main done

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,13 +5,28 @@ import requests from '../Requests';
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
+  /* Set the movies to display only one Results, not an array */
+
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
   useEffect(() => {
     axios.get(requests.requestPopular).then((res) => {
-      setMovies(res.data);
+      setMovies(res.data.results);
     });
   }, []);
-  console.log(movies);
-  return <div>Main</div>;
+  console.log(movie);
+  return (
+    <div className="w-fulll h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
