284440b7f306972fdc9d9bba92519857e8a24803
janisollo <sollo.jani@gmail.com>
2022-09-05 08:06:29 +0700

    Movie api set-up with axios

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import requests from '../Requests';
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((res) => {
+      setMovies(res.data);
+    });
+  }, []);
+  console.log(movies);
   return <div>Main</div>;
 };
 
