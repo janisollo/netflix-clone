7042d014953ad04006b9efabc6ca8404c6911071
janisollo <sollo.jani@gmail.com>
2022-09-09 11:33:42 +0700

    Firebase Auth started. Authcontext.js created.

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from 'firebase/auth';
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -14,4 +16,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = firebase.auth();
